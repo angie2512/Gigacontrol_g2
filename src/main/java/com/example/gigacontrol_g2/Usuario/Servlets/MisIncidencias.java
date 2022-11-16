b3aa7d89da89a9e8e220bdb39c323ef41951cc12
abigail200616 <abigail.ramirez@pucp.edu.pe>
2022-11-16 18:36:54 -0500

    abigail Mis Incidencias Servlet

--- src/main/java/com/example/gigacontrol_g2/Usuario/Servlets/MisIncidencias.java
+++ src/main/java/com/example/gigacontrol_g2/Usuario/Servlets/MisIncidencias.java
@@ -1,16 +1,30 @@
 package com.example.gigacontrol_g2.Usuario.Servlets;
 
+import com.example.gigacontrol_g2.beans.Incidencia;
+import com.example.gigacontrol_g2.daos.DaoDatosFijos;
+import com.example.gigacontrol_g2.daos.SeguridadDao;
+import com.example.gigacontrol_g2.daos.UsersDao;
+import com.example.gigacontrol_g2.daos.UsersDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "MisIncidencias", value = "/MisIncidencias")
 public class MisIncidencias extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action") == null ? "lista" : request.getParameter("action");
+        DaoDatosFijos daoDatosFijos = new DaoDatosFijos();
+        UsersDao usersDao = new UsersDao();
+        SeguridadDao seguridadDao = new SeguridadDao();
+        RequestDispatcher view;
+        //asignar
+        request.setAttribute("ListaDeIncidencias", usersDao.obtenerListaDeIncidencias());
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("Usuario/MisIncidencias.jsp");
+        //enviar
         requestDispatcher.forward(request, response);
 
     }
