e7d560a5aaf5c5d755e5172d860509a9b3e0113c
Manuel <a20185361@pucp.edu.pe>
2022-12-01 11:51:56 -0500

    buscador seguridad

--- src/main/java/com/example/gigacontrol_g2/Servlets/ServletSeguridadBuscarIncidencia.java
+++ src/main/java/com/example/gigacontrol_g2/Servlets/ServletSeguridadBuscarIncidencia.java
@@ -1,11 +1,13 @@
 package com.example.gigacontrol_g2.Servlets;
 
+import com.example.gigacontrol_g2.beans.BUsuarios;
 import com.example.gigacontrol_g2.beans.Incidencia;
 import com.example.gigacontrol_g2.daos.SeguridadDao;
 import com.example.gigacontrol_g2.daos.UsersDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.springframework.lang.UsesSunMisc;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,13 +24,21 @@ public class ServletSeguridadBuscarIncidencia extends HttpServlet {
         String action = request.getParameter("action") == null ? "lista" : request.getParameter("action");
         SeguridadDao seguridadDao = new SeguridadDao();
 
+        UsersDao usersDao = new UsersDao();
+
         switch (action) {
             case "buscar":
                 String searchText = request.getParameter("searchText");
-
+                ArrayList<BUsuarios> usuario = usersDao.buscarPorApellido(searchText);
                 ArrayList<Incidencia> lista = seguridadDao.buscarPorIncidencia(searchText);
 
-                request.setAttribute("lista", lista);
+                request.setAttribute("listaIncidencias", lista);
+                request.setAttribute("listaApellido",usuario);
+
+                for(Incidencia in: lista){
+                    System.out.println("Nombre de Incidencia: "+in.getNombreDeIncidencia());
+                    System.out.println("Usuario: "+in.getUsuario().getNombre());
+                }
                 request.setAttribute("searchText", searchText);
 
                 RequestDispatcher requestDispatcher = request.getRequestDispatcher("Seguridad/InicioSeguridad.jsp");
